e26fc1ce180e95d50f75da29e9d7580ac7a2f477
cortezz2001 <cortezz2001@gmail.com>
2025-05-10 15:51:52 +0500

    Replace ScrollView with FlatList for improved performance and implement NewsCard component for better code organization

--- app/(tabs)/home/news/index.jsx
+++ app/(tabs)/home/news/index.jsx
@@ -3,7 +3,7 @@ import {
     View,
     Text,
     TouchableOpacity,
-    ScrollView,
+    FlatList,
     Image,
     RefreshControl,
 } from "react-native";
@@ -29,6 +29,84 @@ const EmptyStateMessage = ({ searchText }) => {
     );
 };
 
+const NewsCard = ({ item, onPress, i18n }) => (
+    <TouchableOpacity
+        className="rounded-lg mb-4 shadow-md bg-ghostwhite border border-gray-200 overflow-hidden"
+        onPress={onPress}
+        activeOpacity={0.7}
+    >
+        <Image
+            source={{ uri: item.imageUrl }}
+            className="w-full h-48 rounded-t-lg"
+            resizeMode="cover"
+        />
+
+        <View className="p-4">
+            <Text
+                className="font-mmedium text-lg text-gray-800"
+                numberOfLines={2}
+            >
+                {item.title[i18n.language] || item.title.en}
+            </Text>
+            <Text
+                className="font-mregular text-sm text-gray-600 mt-2"
+                numberOfLines={3}
+            >
+                {item.shortDescription[i18n.language] ||
+                    item.shortDescription.en}
+            </Text>
+            <View className="flex-row items-center mt-4 justify-between">
+                <View className="flex-row items-center">
+                    <MaterialIcons name="category" size={16} color="#6B7280" />
+                    <Text className="text-gray-500 ml-1 text-sm">
+                        {item.categoryName[i18n.language] ||
+                            item.categoryName.en}
+                    </Text>
+                </View>
+                <View className="flex-row items-center">
+                    <MaterialIcons
+                        name="access-time"
+                        size={16}
+                        color="#6B7280"
+                    />
+                    <Text className="text-gray-500 ml-1 text-sm">
+                        {new Date(item.createdAt.toDate()).toLocaleDateString(
+                            i18n.language
+                        )}
+                    </Text>
+                </View>
+            </View>
+
+            <View className="flex-row justify-between items-center pt-3 mr-2">
+                <View className="flex-row items-center">
+                    <View className="bg-gray-100 p-1.5 rounded-full">
+                        <MaterialIcons
+                            name="visibility"
+                            size={16}
+                            color="#3B82F6"
+                        />
+                    </View>
+                    <Text className="text-gray-600 ml-2 font-mmedium text-sm">
+                        {item.viewCount || 0}
+                    </Text>
+                </View>
+                <View className="flex-row items-center">
+                    <View className="bg-gray-100 p-1.5 rounded-full">
+                        <MaterialIcons
+                            name="comment"
+                            size={16}
+                            color="#3B82F6"
+                        />
+                    </View>
+                    <Text className="text-gray-600 ml-2 font-mmedium text-sm">
+                        {item.commentCount || 0}
+                    </Text>
+                </View>
+            </View>
+        </View>
+    </TouchableOpacity>
+);
+
 const NewsTab = () => {
     const { t, i18n } = useTranslation();
     const [searchText, setSearchText] = useState("");
@@ -49,16 +127,35 @@ const NewsTab = () => {
         );
     };
 
-    // Функция для обработки pull-to-refresh
     const onRefresh = async () => {
         setRefreshing(true);
         try {
-            await fetchNews(); // Вызываем fetchNews для обновления новостей
+            await fetchNews();
         } catch (err) {
             console.error("Error refreshing news:", err);
         } finally {
             setRefreshing(false);
         }
+    };
+
+    const renderEmptyList = () => {
+        if (searchText) {
+            return <EmptyStateMessage searchText={searchText} />;
+        }
+
+        return (
+            <View className="flex-1 items-center justify-center py-10 bg-secondary">
+                <MaterialIcons name="info" size={64} color="#9CA3AF" />
+                <Text className="text-gray-400 text-lg font-mmedium mt-4 text-center">
+                    {t("no_news_available")}
+                </Text>
+            </View>
+        );
+    };
+
+    const handleNewsPress = async (item) => {
+        router.push(`/pages/news-details/${item.id}`);
+        await updateNewsViewCount(item.id);
     };
 
     if (newsLoading && !refreshing) {
@@ -83,9 +180,18 @@ const NewsTab = () => {
                 tabName="news"
             />
 
-            <ScrollView
+            <FlatList
+                data={getFilteredNews()}
+                renderItem={({ item }) => (
+                    <NewsCard
+                        item={item}
+                        onPress={() => handleNewsPress(item)}
+                        i18n={i18n}
+                    />
+                )}
+                keyExtractor={(item) => item.id}
                 showsVerticalScrollIndicator={false}
-                className="flex-1"
+                ListEmptyComponent={renderEmptyList}
                 refreshControl={
                     <RefreshControl
                         refreshing={refreshing}
@@ -95,104 +201,7 @@ const NewsTab = () => {
                         progressBackgroundColor="#FFFFFF"
                     />
                 }
-            >
-                {searchText && getFilteredNews().length === 0 ? (
-                    <EmptyStateMessage searchText={searchText} />
-                ) : getFilteredNews().length === 0 ? (
-                    <View className="flex-1 items-center justify-center py-10 bg-secondary">
-                        <MaterialIcons name="info" size={64} color="#9CA3AF" />
-                        <Text className="text-gray-400 text-lg font-mmedium mt-4 text-center">
-                            {t("no_news_available")}
-                        </Text>
-                    </View>
-                ) : (
-                    getFilteredNews().map((item) => (
-                        <TouchableOpacity
-                            key={item.id}
-                            className="rounded-lg mb-4 shadow-md bg-ghostwhite border border-gray-200 overflow-hidden"
-                            onPress={async () => {
-                                router.push(`/pages/news-details/${item.id}`);
-                                await updateNewsViewCount(item.id);
-                            }}
-                            activeOpacity={0.7}
-                        >
-                            <Image
-                                source={{ uri: item.imageUrl }}
-                                className="w-full h-48 rounded-t-lg"
-                                resizeMode="cover"
-                            />
-
-                            <View className="p-4">
-                                <Text
-                                    className="font-mmedium text-lg text-gray-800"
-                                    numberOfLines={2}
-                                >
-                                    {item.title[i18n.language] || item.title.en}
-                                </Text>
-                                <Text
-                                    className="font-mregular text-sm text-gray-600 mt-2"
-                                    numberOfLines={3}
-                                >
-                                    {item.shortDescription[i18n.language] ||
-                                        item.shortDescription.en}
-                                </Text>
-                                <View className="flex-row items-center mt-4 justify-between">
-                                    <View className="flex-row items-center">
-                                        <MaterialIcons
-                                            name="category"
-                                            size={16}
-                                            color="#6B7280"
-                                        />
-                                        <Text className="text-gray-500 ml-1 text-sm">
-                                            {item.categoryName[i18n.language] ||
-                                                item.categoryName.en}
-                                        </Text>
-                                    </View>
-                                    <View className="flex-row items-center">
-                                        <MaterialIcons
-                                            name="access-time"
-                                            size={16}
-                                            color="#6B7280"
-                                        />
-                                        <Text className="text-gray-500 ml-1 text-sm">
-                                            {new Date(
-                                                item.createdAt.toDate()
-                                            ).toLocaleDateString(i18n.language)}
-                                        </Text>
-                                    </View>
-                                </View>
-
-                                <View className="flex-row justify-between items-center pt-3 mr-2">
-                                    <View className="flex-row items-center">
-                                        <View className="bg-gray-100 p-1.5 rounded-full">
-                                            <MaterialIcons
-                                                name="visibility"
-                                                size={16}
-                                                color="#3B82F6"
-                                            />
-                                        </View>
-                                        <Text className="text-gray-600 ml-2 font-mmedium text-sm">
-                                            {item.viewCount || 0}
-                                        </Text>
-                                    </View>
-                                    <View className="flex-row items-center">
-                                        <View className="bg-gray-100 p-1.5 rounded-full">
-                                            <MaterialIcons
-                                                name="comment"
-                                                size={16}
-                                                color="#3B82F6"
-                                            />
-                                        </View>
-                                        <Text className="text-gray-600 ml-2 font-mmedium text-sm">
-                                            {item.commentCount || 0}
-                                        </Text>
-                                    </View>
-                                </View>
-                            </View>
-                        </TouchableOpacity>
-                    ))
-                )}
-            </ScrollView>
+            />
         </View>
     );
 };
